68e32fb0877b8c7000c7bdd4a050aa2e239e00b1
Mohamed Hassanin <mhassani@mathworks.com>
2023-07-16 23:53:07 -0400

    one

---
+++ src/data/resume.md
@@ -0,0 +1,83 @@
+Here is the resume converted to Markdown with additional details:
+
+# Mohamed K Hassanin
+
+**Email:** hassanin@udel.edu
+
+## Summary
+
+Passionate software engineer with over 9 years of experience developing enterprise applications, systems software, and distributed cloud systems. Expertise in designing and implementing core components of cybersecurity, distributed systems, and cloud products. Adept at leading teams to successfully build and launch new products and features. Strong communication, mentoring, and evangelizing skills.
+
+## Technical Skills
+
+- Languages: Java, C#, Python, JavaScript, Rust, MATLAB
+- Platforms: AWS, Azure, Kubernetes, Linux, Kafka, Zookeeper 
+- Concepts: Distributed Systems, Cloud Infrastructure, Cybersecurity
+
+## Professional Experience
+
+**Microsoft**
+*Senior Software Engineer*
+**Jan 2022 - Present | Greater Boston Area**
+
+- Collaborated with financial services customers to create highly scalable and resilient solutions leveraging Microsoft and Azure technologies. 
+- Evangelized latest Microsoft and Azure technologies and promoted best engineering practices to customers.
+- Contributed to open source Microsoft projects.
+- Mentored and guided junior engineers in their technical and professional development.
+
+**Akamai Technologies**
+*Principal Software Engineer* 
+**Oct 2020 - Dec 2021 | Greater Boston Area**
+
+- Developed innovative passwordless authentication technologies for Akamai's Enterprise Security product.
+- Led engineering team to successfully build Akamai MFA product from the ground up and launch it.
+- Implemented WebAuthn and push notification features.
+- Built monitoring, analytics, and alerting systems on Azure services including CosmosDB, Time Series Insights, and Stream Analytics.
+
+**Ferrum College**
+*Adjunct Faculty*
+**Aug 2021 - May 2022 | Ferrum, VA** 
+
+- Taught undergraduate computer science courses on databases, algorithms, data structures, and troubleshooting.
+
+**MathWorks** 
+*Senior Software Engineer*
+**Aug 2016 - Oct 2020 | Greater Boston Area**
+
+- Integrated MATLAB products with cloud platforms like Azure to create new SaaS offerings.
+- Led development of offering MATLAB Production Server on Azure as PaaS.
+- Implemented streaming integrations between MATLAB, Kafka, Azure Service Bus.
+- Managed and mentored team of 3 junior developers.
+
+## Education
+
+- **PhD in Electrical and Computer Engineering**, University of Delaware, 2018
+  - Dissertation: Analog Joint Source Channel Coding for Multi-Terminal and Non-Linear Channels
+- **MS in Electrical and Computer Engineering (4.0 GPA)**, University of Delaware, 2015
+  - Thesis: Non-Linear Joint Source Channel Coding for Multi-user Networks
+- **BS in Electronics Engineering (Magna Cum Laude)**, American University in Cairo, 2011
+  - Minors in Computer Science and Mathematics
+
+## Publications
+
+- 6 Peer-Reviewed Conference and Journal Papers
+- 8 US Patents
+
+## Other Experience
+
+- **Quality Engineer**, MathWorks (2015 - 2016)
+- **Wireless Systems Engineering Intern**, Intel (2015) 
+- **Graduate Researcher**, University of Delaware (2012 - 2014)
+- **Engineering Intern**, InterDigital Communications (2014)
+- **Software Development Engineer**, Mentor Graphics (2011 - 2012) 
+- **Undergraduate Teaching Assistant**, American Univ. in Cairo (2009 - 2011)
+
+## Awards
+
+- First prize in University of Delaware ECE Research Day (2020)
+- IEEE Signal Processing Society Best Student Paper Award (2015)  
+
+## Leadership
+
+- Member of University of Delaware ECE PhD Council
+- Reviewer for IEEE journals and conferences
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,50 +1,94 @@
-import React from 'react';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// import Main from '../layouts/Main';
+
+// import Education from '../components/Resume/Education';
+// import Experience from '../components/Resume/Experience';
+// import Skills from '../components/Resume/Skills';
+// import Courses from '../components/Resume/Courses';
+// import References from '../components/Resume/References';
+
+// import courses from '../data/resume/courses';
+// import degrees from '../data/resume/degrees';
+// import work from '../data/resume/work';
+// import { skills, categories } from '../data/resume/skills';
+
+// // NOTE: sections are displayed in order defined.
+// const sections = {
+//   Education: () => <Education data={degrees} />,
+//   Experience: () => <Experience data={work} />,
+//   Skills: () => <Skills skills={skills} categories={categories} />,
+//   Courses: () => <Courses data={courses} />,
+//   References: () => <References />,
+// };
+
+// const Resume = () => (
+//   <Main
+//     title="Resume"
+//     description="Mohamed Hassanin"
+//   >
+//     <article className="post" id="resume">
+//       <header>
+//         <div className="title">
+//           <h2><Link to="resume">Resume</Link></h2>
+//           <div className="link-container">
+//             {Object.keys(sections).map((sec) => (
+//               <h4 key={sec}>
+//                 <a href={`#${sec.toLowerCase()}`}>{sec}</a>
+//               </h4>))}
+//           </div>
+//         </div>
+//       </header>
+//       {Object.entries(sections).map(([name, Section]) => (
+//         <Section key={name} />
+//       ))}
+//     </article>
+//   </Main>
+// );
+
+// export default Resume;
+
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Markdown from 'markdown-to-jsx';
 
 import Main from '../layouts/Main';
 
-import Education from '../components/Resume/Education';
-import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
-import References from '../components/Resume/References';
+const Resume = () => {
+  const [markdown, setMarkdown] = useState('');
 
-import courses from '../data/resume/courses';
-import degrees from '../data/resume/degrees';
-import work from '../data/resume/work';
-import { skills, categories } from '../data/resume/skills';
+  useEffect(() => {
+    import('../data/resume.md')
+      .then((res) => {
+        fetch(res.default)
+          .then((r) => r.text())
+          .then(setMarkdown);
+      });
+  });
 
-// NOTE: sections are displayed in order defined.
-const sections = {
-  Education: () => <Education data={degrees} />,
-  Experience: () => <Experience data={work} />,
-  Skills: () => <Skills skills={skills} categories={categories} />,
-  Courses: () => <Courses data={courses} />,
-  References: () => <References />,
-};
+  const count = markdown.split(/\s+/)
+    .map((s) => s.replace(/\W/g, ''))
+    .filter((s) => s.length).length;
 
-const Resume = () => (
-  <Main
-    title="Resume"
-    description="Michael D'Angelo's Resume. Smile Identity, Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet, and Facebook."
-  >
-    <article className="post" id="resume">
-      <header>
-        <div className="title">
-          <h2><Link to="resume">Resume</Link></h2>
-          <div className="link-container">
-            {Object.keys(sections).map((sec) => (
-              <h4 key={sec}>
-                <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-              </h4>))}
+  return (
+    <Main
+      title="About"
+      description="Learn about Mohamed Hassanin"
+    >
+      <article className="post markdown" id="about">
+        <header>
+          <div className="title">
+            <h2><Link to="/about">About Me</Link></h2>
+            <p>(in about {count} words)</p>
           </div>
-        </div>
-      </header>
-      {Object.entries(sections).map(([name, Section]) => (
-        <Section key={name} />
-      ))}
-    </article>
-  </Main>
-);
+        </header>
+        <Markdown>
+          {markdown}
+        </Markdown>
+      </article>
+    </Main>
+  );
+};
 
 export default Resume;
